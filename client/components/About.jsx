71a845351095f9cf67fa87dddc0b4633f4113e24
Krissy Do <krissytdo@hotmail.com>
2020-07-06 21:25:12 -0700

    add get query param func, more css, 2nd map obj

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,35 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Map from './Map.jsx';
-const About = ({children, onClose, open}) => 
+// import styled from 'styled-components';
+
+const About = ({city, state, country, lat, lng, onClose, gettingAround, description, open}) => {
+  
+
+return (
   
   open ?
   ReactDOM.createPortal(
    
-      <div className='overlay' style={{    
-      position: 'fixed',
-      top: '0',
-      background: 'rgba(0,0,0, 0.4)',
-      width: '100%',
-      height: '100%',
-      left: '0',
-    }}>
-        <div className='content' style={
-          { width: '90%',
-            borderRadius: '5px',
-            background: 'white',
-            padding: '20px',
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)'
-        }}>
+    <div className='overlay' style={{    
+    position: 'fixed',
+    top: '0',
+    background: 'rgba(0,0,0, 0.5)',
+    width: '100%',
+    height: '100%',
+    left: '0',
+  }}>
+      <div className='content' style={
+        { width: '60%',
+          borderRadius: '5px',
+          background: 'white',
+          padding: '20px',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+      }}>
           <div id='modal_close' onClick={onClose}>&times;</div>
-          <div>Map should be here</div>
+          <section className="indent" style={{float: 'left'}}>
+            <section style={{float: 'left', width: '50%'}}>
+              <h2>Location</h2>
+              <h3>{city}, {state}, {country}</h3>
+              <p>{description}</p>
+              <h3>Getting Around</h3>
+              <p>{gettingAround !== null ? gettingAround : null}</p>
+            </section>
+            <section style={{float: 'left', width: '50%'}}>
+              <Map id={'map-modal'} style={{width: '100%', height: '500px'}} lat={lat} lng={lng}/>
+            </section>
+          </section>
+                
+           
+           
         </div> 
   </div>,
   document.getElementById('about-modal'))
   : null
-
-
+)
+    }
 export default About;
--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:1000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
